4ee8d4b0744cbda0a03205146fdb662864785531
Guillermo G. Kelly S <malak_ai@hotmail.com>
2021-10-05 01:22:52 -0500

    tic-tac-toe

---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Square(props) {
+    return (
+        <button className = {props.value?"btn disabled":"btn"} onClick = { props.onClick }>
+            { props.value }
+        </button>
+    );
+}
